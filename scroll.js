49f96337152679a1dc7c4de3bbb5ab78dfeb9113
leon in 't Veld <leon3110l@gmail.com>
2017-03-25 00:35:26 +0100

    fixed browser support, but it still doesn't work great

--- scroll.js
+++ scroll.js
@@ -12,9 +12,11 @@ function scrollTo(scrollTo, speed) {
       }
     }
     if (typeof(scrollTo) != "number") {
-      scrollTo = getScrollElement();
+      scrollTo = scrollTo.offsetTop;
+      console.log(scrollTo);
     }
     var start = getScrollElement();
+    console.log(start);
     if (start == scrollTo) {
       return;
     } else {
@@ -40,6 +42,7 @@ function scrollTo(scrollTo, speed) {
         }
         if (getScrollElement() === scrollTo) {
           // done
+          console.log("done");
           isPageScrolling = false;
           return;
         }
@@ -50,23 +53,142 @@ function scrollTo(scrollTo, speed) {
   }
 }
 
+function scrollToLR(scrollTo, speed) {
+  if (!isPageScrolling) {
+    isPageScrolling = true;
+    if (!speed) {
+      if (window.matchMedia("(max-width: 599px)").matches) {
+        speed = 25;
+      } else {
+        // the viewport is less than 599 pixels wide
+        speed = 50;
+      }
+    }
+    if (typeof(scrollTo) != "number") {
+      scrollTo = scrollTo.offsetLeft;
+      console.log(scrollTo);
+    }
+    var start = getScrollElement(true);
+    console.log(start);
+    if (start == scrollTo) {
+      return;
+    } else {
+      var diff;
+      var close = false;
+      function scroll() {
+        diff = getScrollElement(true) - scrollTo;
+        if (diff < speed && diff > -speed) {
+          close = true;
+        }
+        if (diff < 0) {
+          if (close) {
+            setScrollElement(getScrollElement(true)+1, true);
+          } else {
+            setScrollElement(getScrollElement(true)+speed, true);
+          }
+        } else {
+          if (close) {
+            setScrollElement(getScrollElement(true)-1, true);
+          } else {
+            setScrollElement(getScrollElement(true)-speed, true);
+          }
+        }
+        if (getScrollElement(true) === scrollTo) {
+          // done
+          console.log("done");
+          isPageScrolling = false;
+          return;
+        }
+        requestAnimationFrame(scroll);
+      }
+      scroll();
+    }
+  }
+}
 
+var bestScrollElement;
 function getScrollElement(left) {
   if (left) {
-    return window.pageXOffset || document.documentElement.scrollLeft || document.body.scrollLeft || 0;
+    if (!bestScrollElement) {
+      if (document.documentElement.scrollLeft) {
+        bestScrollElement = 0;
+      } else if (document.body.scrollLeft) {
+        bestScrollElement = 1;
+      } else if (window.pageXOffset) {
+        bestScrollElement = 2;
+      } else {
+        return 0
+      }
+    }
+    switch (bestScrollElement) {
+      case 0:
+        return document.documentElement.scrollLeft;
+        break;
+      case 1:
+        return document.body.scrollLeft;
+        break;
+      case 2:
+        return window.pageXOffset;
+        break;
+      default:
+        return 0
+    }
   } else {
-    return window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+    if (!bestScrollElement) {
+      if (document.documentElement.scrollTop) {
+        bestScrollElement = 0;
+      } else if (document.body.scrollTop) {
+        bestScrollElement = 1;
+      } else if (window.pageYOffset) {
+        bestScrollElement = 2;
+      } else {
+        return 0
+      }
+    }
+    switch (bestScrollElement) {
+      case 0:
+        return document.documentElement.scrollTop;
+        break;
+      case 1:
+        return document.body.scrollTop;
+        break;
+      case 2:
+        return window.pageYOffset;
+        break;
+      default:
+        return 0
+    }
   }
 }
 
 function setScrollElement(x, left) {
   if (left) {
-    window.pageXOffset = x;
-    document.documentElement.scrollLeft = x;
-    document.body.scrollLeft = x;
+    switch (bestScrollElement) {
+      case 0:
+        document.documentElement.scrollLeft = x;
+        break;
+      case 1:
+        document.body.scrollLeft = x;
+        break;
+      case 2:
+        window.pageXOffset = x;
+        break;
+      default:
+        return 0
+    }
   } else {
-    window.pageYOffset = x;
-    document.documentElement.scrollTop = x;
-    document.body.scrollTop = x;
+    switch (bestScrollElement) {
+      case 0:
+        document.documentElement.scrollTop = x;
+        break;
+      case 1:
+        document.body.scrollTop = x;
+        break;
+      case 2:
+        window.pageYOffset = x;
+        break;
+      default:
+        return 0
+    }
   }
 }
