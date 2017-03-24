7c0c60234193b9091d4e29ff6f15e30ab8af40c8
leon in 't Veld <leon3110l@gmail.com>
2017-03-24 22:24:04 +0100

    added left to right scrolling

--- scroll.js
+++ scroll.js
@@ -49,3 +49,53 @@ function scrollTo(scrollTo, speed) {
     }
   }
 }
+
+function scrollToLR(scrollTo, speed) {
+  if (!isPageScrolling) {
+    isPageScrolling = true;
+    if (!speed) {
+      if (window.matchMedia("(max-width: 599px)").matches) {
+        speed = 25;
+      } else {
+        // the viewport is less than 599 pixels wide
+        speed = 50;
+      }
+    }
+    if (typeof(scrollTo) != "number") {
+      scrollTo = scrollTo.offsetLeft;
+    }
+    var start = document.body.scrollLeft;
+    if (start == scrollTo) {
+      return;
+    } else {
+      var diff;
+      var close = false;
+      function scroll() {
+        diff = document.body.scrollLeft - scrollTo;
+        if (diff < speed && diff > -speed) {
+          close = true;
+        }
+        if (diff < 0) {
+          if (close) {
+            document.body.scrollLeft++;
+          } else {
+            document.body.scrollLeft += speed;
+          }
+        } else {
+          if (close) {
+            document.body.scrollLeft--;
+          } else {
+            document.body.scrollLeft -= speed;
+          }
+        }
+        if (document.body.scrollLeft === scrollTo) {
+          // done
+          isPageScrolling = false;
+          return;
+        }
+        requestAnimationFrame(scroll);
+      }
+      scroll();
+    }
+  }
+}
