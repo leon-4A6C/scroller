1eb4be31b71957403caa44980cfbb1da519a948e
leon in 't Veld <leon3110l@gmail.com>
2017-03-24 22:18:00 +0100

    removed a bug

--- scroll.js
+++ scroll.js
@@ -1,90 +1,50 @@
 // my own smoothScrolling function
+var isPageScrolling = false;
 function scrollTo(scrollTo, speed) {
-  if (!speed) {
-    if (window.matchMedia("(max-width: 599px)").matches) {
-      speed = 25;
-    } else {
-      /* the viewport is less than 400 pixels wide */
-      speed = 50;
-    }
-  }
-  if (typeof(scrollTo) != "number") {
-    scrollTo = scrollTo.offsetTop;
-  }
-  var start = document.body.scrollTop;
-  if (start == scrollTo) {
-    return;
-  } else {
-    var diff;
-    var close = false;
-    function scroll() {
-      diff = document.body.scrollTop - scrollTo;
-      if (diff < speed && diff > -speed) {
-        close = true;
-      }
-      if (diff < 0) {
-        if (close) {
-          document.body.scrollTop++;
-        } else {
-          document.body.scrollTop += speed;
-        }
+  if (!isPageScrolling) {
+    isPageScrolling = true;
+    if (!speed) {
+      if (window.matchMedia("(max-width: 599px)").matches) {
+        speed = 25;
       } else {
-        if (close) {
-          document.body.scrollTop--;
-        } else {
-          document.body.scrollTop -= speed;
-        }
+        /* the viewport is less than 400 pixels wide */
+        speed = 50;
       }
-      if (document.body.scrollTop === scrollTo) {
-        return;
-      }
-      requestAnimationFrame(scroll);
     }
-    scroll();
-  }
-}
-
-function scrollLRTo(scrollTo, speed) {
-  if (!speed) {
-    if (window.matchMedia("(max-width: 599px)").matches) {
-      speed = 25;
+    if (typeof(scrollTo) != "number") {
+      scrollTo = scrollTo.offsetTop;
+    }
+    var start = document.body.scrollTop;
+    if (start == scrollTo) {
+      return;
     } else {
-      /* the viewport is less than 400 pixels wide */
-      speed = 50;
-    }
-  }
-  if (typeof(scrollTo) != "number") {
-    scrollTo = scrollTo.offsetTop;
-  }
-  var start = document.body.scrollLeft;
-  if (start == scrollTo) {
-    return;
-  } else {
-    var diff;
-    var close = false;
-    function scroll() {
-      diff = document.body.scrollLeft - scrollTo;
-      if (diff < speed && diff > -speed) {
-        close = true;
-      }
-      if (diff < 0) {
-        if (close) {
-          document.body.scrollLeft++;
-        } else {
-          document.body.scrollLeft += speed;
+      var diff;
+      var close = false;
+      function scroll() {
+        diff = document.body.scrollTop - scrollTo;
+        if (diff < speed && diff > -speed) {
+          close = true;
         }
-      } else {
-        if (close) {
-          document.body.scrollLeft--;
+        if (diff < 0) {
+          if (close) {
+            document.body.scrollTop++;
+          } else {
+            document.body.scrollTop += speed;
+          }
         } else {
-          document.body.scrollLeft -= speed;
+          if (close) {
+            document.body.scrollTop--;
+          } else {
+            document.body.scrollTop -= speed;
+          }
         }
+        if (document.body.scrollTop === scrollTo) {
+          isPageScrolling = false;
+          return;
+        }
+        requestAnimationFrame(scroll);
       }
-      if (document.body.scrollLeft === scrollTo) {
-        return;
-      }
-      requestAnimationFrame(scroll);
+      scroll();
     }
-    scroll();
   }
 }
